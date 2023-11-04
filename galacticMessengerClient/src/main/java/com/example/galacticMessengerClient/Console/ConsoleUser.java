d423eb2eb5fa722dcdc73d229fcad2e176115d2d
cyril <cyril.porez@laplateforme.io>
2023-11-04 15:05:40 +0100

    fix coté client mise en place de la console

---
+++ galacticMessengerClient/src/main/java/com/example/galacticMessengerClient/Console/ConsoleUser.java
@@ -0,0 +1,75 @@
+package com.example.galacticMessengerClient.Console;
+
+// import org.yaml.snakeyaml.scanner.Scanner;
+import java.util.Scanner;
+
+import com.example.galacticMessengerClient.Request.RequestApi;
+
+public class ConsoleUser {
+  public ConsoleUser() {
+
+  }
+
+  public void displayLaunchInstruction() {
+    System.out.println("==================");
+		System.out.println("GALACTIC MESSENGER");
+		System.out.println("==================");
+		System.out.println("Bienvenue sur galactic Messenger.");
+		System.out.println();
+		System.out.println("Afin d'utiliser l'application, voici les commandes");
+		System.out.println();
+		System.out.println("Inscription:");
+		System.out.println("- /register \"nom_d'utilisateur\" \"mot_de_passe\"");
+		System.out.println("Connexion:");
+		System.out.println("- /login \"nom_d'utilisateur\" \"mot_de_passe\"");
+		System.out.println("Demander de l'aide:");
+		System.out.println("- /help");
+		System.out.println();
+  }
+
+  public static void help() {
+		System.out.println("Afin de vous aider à utiliser l'application voici la liste de toutes les comandes:");
+		System.out.println("Inscription:");
+		System.out.println("- /register \"nom_d'utilisateur\" \"mot_de_passe\"");
+		System.out.println("Connexion:");
+		System.out.println("- /login \"nom_d'utilisateur\" \"mot_de_passe\"");
+
+	}
+
+  public void ConsoleUseGalacticMessenger(String[] args) {
+    RequestApi requestApi = new RequestApi();
+    String addressIp = args[0];
+		String port = args[1];
+		String adressServer = addressIp + ":" + port;
+    Scanner scanner = new Scanner(System.in);
+		System.out.println("Veuillez entrer votre commande!");
+		
+		boolean isRunning = true;
+
+		while(isRunning) {
+			String command = scanner.nextLine();
+			String[] commandSplit = command.split(" ");
+		  String choiceCommand = commandSplit[0];
+			switch (choiceCommand) {
+				case "/register":
+					requestApi.request(commandSplit[1], commandSplit[2], adressServer, commandSplit[0]);
+					break;
+				case "/login":
+          requestApi.request(commandSplit[1], commandSplit[2], adressServer, commandSplit[0]);
+					// request(commandSplit[1], commandSplit[2], adressServer, commandSplit[0]);
+					break;
+				case "/help":
+					help();
+					break;
+			
+				default:
+					System.out.println("Commande non reconnus par le système !");
+					break;
+			}
+		}
+
+		scanner.close();
+  }
+
+
+}
