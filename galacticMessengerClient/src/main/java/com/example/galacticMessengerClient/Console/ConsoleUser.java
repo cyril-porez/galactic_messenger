9bb90dc5ea709c8529fff75dd9a0898d87211de1
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2023-11-19 17:07:59 +0100

    refactor : meilleure gestion des returns dans "getDataFromJWT"

--- galacticMessengerClient/src/main/java/com/example/galacticMessengerClient/Console/ConsoleUser.java
+++ galacticMessengerClient/src/main/java/com/example/galacticMessengerClient/Console/ConsoleUser.java
@@ -278,33 +278,30 @@ public class ConsoleUser {
     public String getDataFromJWT(String jwt, String field) {
         // Divise le JWT
         String[] parts = jwt.split("\\.");
-        if (parts.length != 3) {
-            System.out.println("Jeton JWT invalide");
-            return null;
-        }
-
-        ObjectMapper m = new ObjectMapper();
-
-        // Analyse les deux premières parties du JWT
-        for (int i = 0; i < 2; i++) {
-            byte[] partBytes = Base64.getUrlDecoder().decode(parts[i]);
-            String part = new String(partBytes);
-
-            JsonNode partNode;
-            try {
-                partNode = m.readTree(part);
-            } catch (Exception e) {
-                System.out.println("Impossible de lire l'arbre du JWT");
-                return null;
-            }
-
-            JsonNode fieldNode = partNode.get(field);
-
-            if (fieldNode != null) {
-                return fieldNode.asText();
+        if (parts.length == 3) {
+            ObjectMapper m = new ObjectMapper();
+    
+            // Analyse les deux premières parties du JWT
+            for (int i = 0; i < 2; i++) {
+                byte[] partBytes = Base64.getUrlDecoder().decode(parts[i]);
+                String part = new String(partBytes);
+    
+                JsonNode partNode;
+                try {
+                    partNode = m.readTree(part);
+                } catch (Exception e) {
+                    System.out.println("Impossible de lire l'arbre du JWT");
+                    break;
+                }
+    
+                JsonNode fieldNode = partNode.get(field);
+    
+                if (fieldNode != null) {
+                    return fieldNode.asText();
+                }
             }
         }
-
+        
         return null;
     }
 }
