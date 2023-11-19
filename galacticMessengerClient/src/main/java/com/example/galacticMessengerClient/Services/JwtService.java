edee3484f3ef99dead541544ab498943f41f81d4
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2023-11-19 17:12:35 +0100

    refactor : meilleure gestion des returns dans "getDataFromJWT"

--- galacticMessengerClient/src/main/java/com/example/galacticMessengerClient/Services/JwtService.java
+++ galacticMessengerClient/src/main/java/com/example/galacticMessengerClient/Services/JwtService.java
@@ -10,33 +10,31 @@ public class JwtService {
     public String getDataFromJWT(String jwt, String field) {
         // Divise le JWT
         String[] parts = jwt.split("\\.");
-        if (parts.length != 3) {
-            System.out.println("Jeton JWT invalide");
-            return null;
-        }
+        if (parts.length == 3) {
 
-        ObjectMapper m = new ObjectMapper();
+            ObjectMapper m = new ObjectMapper();
 
-        // Analyse les deux premières parties du JWT
-        for (int i = 0; i < 2; i++) {
-            byte[] partBytes = Base64.getUrlDecoder().decode(parts[i]);
-            String part = new String(partBytes);
+            // Analyse les deux premières parties du JWT
+            for (int i = 0; i < 2; i++) {
+                byte[] partBytes = Base64.getUrlDecoder().decode(parts[i]);
+                String part = new String(partBytes);
 
-            JsonNode partNode;
-            try {
-                partNode = m.readTree(part);
-            } catch (Exception e) {
-                System.out.println("Impossible de lire l'arbre du JWT");
-                return null;
-            }
+                JsonNode partNode;
+                try {
+                    partNode = m.readTree(part);
+                } catch (Exception e) {
+                    System.out.println("Impossible de lire l'arbre du JWT");
+                    break; // sors du "for" et retourne null
+                }
 
-            JsonNode fieldNode = partNode.get(field);
+                JsonNode fieldNode = partNode.get(field);
 
-            if (fieldNode != null) {
-                return fieldNode.asText();
+                if (fieldNode != null) {
+                    return fieldNode.asText(); // retourne la valeur du champ recherché
+                }
             }
         }
 
-        return null;
+        return null; // si rien n'a été trouvé
     }
 }
